2f3fef336093bc587819491dcf9cea0f0478f89d
Faisal Rana <inquisitive@gmail.com>
2021-06-19 03:40:43 -0700

    added home controller, accounts controller, views, viewmodel and login and register views

---
+++ SweetShop/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SweetShop.Controllers
+{
+    public class HomeController : Controller
+    {
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        ViewBag.AllSweets = new List<Sweet>(_db.Sweets.OrderBy(sweet => sweet.Name));
+        ViewBag.AllFlavors = new List<Flavor>(_db.Flavors.OrderBy(flavor => flavor.Name));
+        return View();
+      }
+
+    }
+}

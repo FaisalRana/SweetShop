ede7e57bce01af2d824dfed964ce46263079e941
Faisal Rana <inquisitive@gmail.com>
2021-06-20 00:26:13 -0700

    added functionality to add machine and add flavor

--- SweetShop/Controllers/TreatController.cs
+++ SweetShop/Controllers/TreatController.cs
@@ -29,7 +29,7 @@ namespace SweetShop.Controllers
       return View();
     }
     [HttpPost]
-    public async Task<ActionResult> Create(Treat treat, int FlavorId)
+    public ActionResult Create(Treat treat, int FlavorId)
     {
       _db.Treats.Add(treat);
       _db.SaveChanges();
@@ -51,7 +51,7 @@ namespace SweetShop.Controllers
     public ActionResult AddFlavor(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
-      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
       return View(thisTreat);
     }
 
@@ -64,6 +64,28 @@ namespace SweetShop.Controllers
       }
       _db.SaveChanges();
       return RedirectToAction("Details", new { id = Treat.TreatId });
+    }
+    [HttpPost]
+    public ActionResult DeleteFlavor(int joinId)
+    {
+      var thisTreatFlavor = _db.TreatFlavors.FirstOrDefault(entry => entry.TreatFlavorId == joinId);
+      _db.TreatFlavors.Remove(thisTreatFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = thisTreatFlavor.TreatId });
+    }
+    public ActionResult Delete(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index", "Home");
     }
   }
 }
